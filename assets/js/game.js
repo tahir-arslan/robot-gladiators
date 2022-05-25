b2ca76daa9dd782842adc44e00d324a78d4949d9
tahir-arslan <101910849+tahir-arslan@users.noreply.github.com>
2022-05-24 20:08:27 -0400

    added enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,23 @@
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto","Amy Anddroid","Robo Trumble"];
+for(var i=0; i<enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+}
+
 var enemyHealth = 50;
 var enemyAttack = 12;
 
@@ -53,4 +66,6 @@ var fight = function() {
 
 }
 
-fight();
+for (var i=0; i<enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
